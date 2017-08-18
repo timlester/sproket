76e8c93b1019b4cce9460d6b4f5ec1c2b7ed62c7
Timothy Lester <timothy.lester@live.com>
2017-08-17 23:15:44 -0400

    Updated file extension for sprocket files.
    Updated file extension for sprocket files.

--- src/sproket.rs
+++ src/sproket.rs
@@ -3,7 +3,7 @@ use source::*;
 
 pub fn run(source_dir: &str) {
     let mut source_manager = SourceManager::new();
-    let file = &[source_dir, "/hello_world.ja"].join("");
+    let file = &[source_dir, "/hello_world.spr"].join("");
     source_manager.add_source_file(file);
 
     for file in source_manager.source_files {
